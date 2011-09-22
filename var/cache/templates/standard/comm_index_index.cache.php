7e7c2f614808b18b0f3fdfb54095c0ad2b135527
Sebastian Noll <snoll@sn-mini.local>
2011-09-22 15:30:49 +0200

    Remove cache files

--- var/cache/templates/standard/comm_index_index.cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package Recipe PHP Framework
- * Auto-generated cache file for:
- * Template Cache File
- * It is recommended to not modify anything here.
- */
-
-?><?php // Cache-Generator finished ?>
