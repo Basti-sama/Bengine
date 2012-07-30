d8ab06fe0a444fc745faab4b4ac14f8490494da9
Basti-sama <secretchampion@gmail.com>
2012-07-30 19:45:49 +0200

    AI accessibility

--- ai/app/Admin/Controller/Abstract.php
+++ ai/app/Admin/Controller/Abstract.php
@@ -1,6 +1,16 @@
 <?php
 abstract class Admin_Controller_Abstract extends Recipe_Controller_Abstract
 {
-
+	/**
+	 * @return Admin_Controller_Abstract
+	 */
+	protected function init()
+	{
+		if($this->getParam("controller") != "auth" && !Core::getUser()->ifPermissions(array("HAS_AI_ACCESS")))
+		{
+			$this->redirect("auth");
+		}
+		return parent::init();
+	}
 }
 ?>
