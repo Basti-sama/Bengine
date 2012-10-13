142fa0feadc9b26616e8ce2a06b8af7ce605d049
Basti-sama <secretchampion@gmail.com>
2012-10-13 20:32:33 +0200

    Bugfix #0000137

--- app/setup/update_0.20_0.21.sql
+++ app/setup/update_0.20_0.21.sql
@@ -1,2 +1,4 @@
 DELETE FROM `bengine_phrases` WHERE `title` = 'ALLIANCE_ATTACK' AND `languageid` = 1 AND `phrasegroupid` = 1 LIMIT 1;
 DELETE FROM `bengine_phrases` WHERE `title` = 'MENU_EXCHANGE';
+
+UPDATE `bengine_phrases` SET `content` = 'Eine schwache Verteidigungsm&ouml;glichkeit, die einfache Lasersch&uuml;sse auf feindliche Raumschiffe feuert.' WHERE `title` = 'LIGHT_LASER_DESC' AND `languageid` = 1;
