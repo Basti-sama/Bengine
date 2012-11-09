8daad71df23bb8f03e6fc4a03a04424bf0dfbb53
Basti-sama <secretchampion@gmail.com>
2012-11-09 20:11:27 +0100

    Version number updated

--- app/code/Bengine.php
+++ app/code/Bengine.php
@@ -7,7 +7,7 @@
  * @version $Id: Bengine.php 55 2011-08-14 16:50:32Z secretchampion $
  */
 
-define("BENGINE_VERSION", "0.21-dev");
+define("BENGINE_VERSION", "0.21rc1");
 define("BENGINE_REVISION", 21);
 define("VERSION_CHECK_PAGE", "http://bengine.de/version.php");
 
