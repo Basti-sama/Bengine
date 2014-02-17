2feab4bc11b5d0b5d2f7f04708378945035590bc
basti <8OQrn7k56,9T>
2014-02-17 21:22:23 +0100

    Cronjob for galaxy clean up optimized

--- app/code/Bengine/Game/Cronjob/RemoveGalaxyGarbage.php
+++ app/code/Bengine/Game/Cronjob/RemoveGalaxyGarbage.php
@@ -16,14 +16,11 @@ class Bengine_Game_Cronjob_RemoveGalaxyGarbage extends Recipe_CronjobAbstract
 	 */
 	protected function removeGalaxyGarbage()
 	{
-		$result = Core::getQuery()->select("galaxy g", array("g.planetid", "e.eventid"), "LEFT JOIN ".PREFIX."events e ON (e.destination = g.planetid)", "g.destroyed = '1'");
+		$result = Core::getQuery()->select("galaxy g", array("g.planetid"), "LEFT JOIN ".PREFIX."events e ON (e.destination = g.planetid)", "g.destroyed = '1' AND e.eventid IS NULL");
 		foreach($result->fetchAll() as $row)
 		{
-			if(!$row["eventid"])
-			{
-				$id = $row["planetid"];
-				Core::getQuery()->delete("planet", "planetid = ?", null, null, array($id));
-			}
+			$id = $row["planetid"];
+			Core::getQuery()->delete("planet", "planetid = ?", null, null, array($id));
 		}
 		$result->closeCursor();
 		return $this;
